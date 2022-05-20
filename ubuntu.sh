42d9b7501efcd057a73fe25872de833ec3a0f786
vector1one <39304498+vector1one@users.noreply.github.com>
2022-05-20 02:42:20 -0400

    Update and rename ubuntu to ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#bluff need a gui server to boot headless with features.
+#add/remove as you see fit 
+#simple customise env script
+
+#system
+/usr/bin/apt update 
+/usr/bin/apt install -y htop
+/usr/bin/apt install -y net-tools
+/usr/bin/apt install -y open-vm-tools-desktop
+/usr/bin/apt install -y openssh-server
+#openjdk-11-jdk
+/usr/bin/apt install -y docker
+/usr/bin/apt install -y docker-compose
+
+
+#start server init 3
+sudo systemctl set-default multi-user.target
+#reboot for headless or
+#/usr/sbin/init 3
+
