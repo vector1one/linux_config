c3f00de25a43044cd38134c655f3df5d6d40374a
vector1one <39304498+vector1one@users.noreply.github.com>
2024-03-19 22:21:17 -0400

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -9,6 +9,9 @@
 /usr/bin/apt install -y net-tools
 /usr/bin/apt install -y open-vm-tools-desktop
 /usr/bin/apt install -y openssh-server
+/usr/bin/apt install -y curl
+/usr/bin/apt install -y git
+/usr/bin/apt install -y nmap
 #openjdk-11-jdk
 /usr/bin/apt install -y docker
 /usr/bin/apt install -y docker-compose
@@ -19,12 +22,14 @@ sudo systemctl set-default multi-user.target
 #reboot for headless or
 #/usr/sbin/init 3
 
+#
+#still a WIP
 #lvm expander
-cd ~/Downloads
-mkdir Git
-cd Git
-git clone https://github.com/vector1one/diskimage_ubuntu.git
-chmod a+x diskimage_ubuntu.sh
+#cd ~/Downloads
+#mkdir Git
+#cd Git
+#git clone https://github.com/vector1one/diskimage_ubuntu.git
+#chmod a+x diskimage_ubuntu.sh
 
 #adding in smbv1 connection
 #sudo apt install samba
